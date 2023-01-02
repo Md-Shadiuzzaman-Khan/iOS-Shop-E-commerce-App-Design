c5ac33261c675229e3d42c15c595f967b02e64d6
Md. Shadiuzzaman Khan <68236926+Md-Shadiuzzaman-Khan@users.noreply.github.com>
2023-01-03 02:16:30 +0600

    Add files via upload

---
+++ EcommerceApp/EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by MacBOOK Pro on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
